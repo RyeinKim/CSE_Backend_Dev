0ab724df677298936e0c81de2483ae8588c79bcc
RyeinKim <ryein6896@gmail.com>
2023-08-09 16:45:30 +0900

    database

--- config/database.js
+++ config/database.js
@@ -4,7 +4,8 @@ const connection = mysql.createConnection({
     host: '49.247.43.150',
     user: 'wayne',
     password: '12341234aS!',
-    database: 'my_db'
+    database: 'my_db',
+    insecureAuth: true
 });
 
 connection.connect();
