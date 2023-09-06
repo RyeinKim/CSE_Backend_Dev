4052a17ae6fadff977f9b86d7fef5275b3d929c1
Ryein Kim <ryein6896@gmail.com>
2023-09-06 17:32:16 +0900

    Keep dev in Laptop

--- controllers/reply.js
+++ controllers/reply.js
@@ -1,6 +1,7 @@
 const Reply = require('../models/reply');
 const mysql = require('mysql');
 const Post = require("./posts");
+const {devlog} = require("../config/config");
 
 /**
  * 회원가입
@@ -14,11 +15,16 @@ const Post = require("./posts");
 
 // 게시글쓰기
 exports.writeReply = (req, res) => {
-    const { content } = req.body;
-    const { user_id } = req.session; // 로그인한 사용자의 user_id
+    devlog('reply/wrtieReply in');
 
-    if (!content || content === null) {
-        return res.status(400).json({ error: 'Content is required.' });
+    const { reply, post_id } = req.body;
+
+    const user_id = req.session.user_id; // 로그인한 사용자의 user_id
+    devlog(`reply / req.session = ${req.session}`);
+    devlog(`reply / req.session.user_id = ${user_id}`);
+
+    if (!reply || reply === null) {
+        return res.status(400).json({ error: 'Reply content is required.' });
     }
 
     if (!user_id) {
@@ -27,6 +33,8 @@ exports.writeReply = (req, res) => {
 
     // user_id를 사용하여 사용자 정보 조회
     Post.getUserById(user_id, (error, user) => {
+        console.log('reply / getUserById In');
+
         if (error) {
             console.error(error);
             return res.status(500).json({ error: 'An error occurred' });
@@ -40,16 +48,16 @@ exports.writeReply = (req, res) => {
             post_id: mysql.escape(post_id),
             user_id: mysql.escape(user_id),
             username: mysql.escape(user.username), // 사용자 정보에서 username 사용
-            content: mysql.escape(content),
+            reply: mysql.escape(reply),
         }
 
-        Reply.writeReply(reqData, (error, post_id) => {
+        Reply.writeReply(reqData, (error, reply_id) => {
             if (error) {
                 console.error(error);
                 return res.status(500).json({ error: 'An error occurred' });
             }
 
-            return res.status(201).json({ message: `Upload post Success. Post ID is ${post_id}` });
+            return res.status(201).json({ message: `Upload reply Success. Reply ID is ${reply_id}` });
         });
     });
 }
--- models/reply.js
+++ models/reply.js
@@ -3,8 +3,11 @@ const {devlog} = require("../config/config");
 
 // 게시글쓰기
 exports.writeReply = (reqData, callback) => {
+    console.log(`${reqData}`);
+    console.log(`${reqData.post_id} | ${reqData.user_id} | ${reqData.username} | ${reqData.reply}`);
+
     const sql = `INSERT INTO reply (post_id, user_id, username, reply)
-                VALUES (${reqData.author_id}, ${reqData.title}, ${reqData.author}, ${reqData.content});`;
+                VALUES (${reqData.post_id}, ${reqData.user_id}, ${reqData.username}, ${reqData.reply});`;
     mysql.connection.query(sql, (error, results) => {
         if (error)  {
             // 에러
