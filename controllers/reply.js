5f531a897756d99734d401af8fe9d592cd86c10a
RyeinKim <ryein6896@gmail.com>
2023-09-22 02:39:26 +0900

    Keep dev on S700

--- controllers/reply.js
+++ controllers/reply.js
@@ -1,119 +1,8 @@
 const Reply = require('../models/reply');
 const mysql = require('mysql');
-const {devlog} = require("../config/config");
+const {devlog, errorlog} = require("../config/config");
 const db = require("../config/database");
 const replyUtils = require("../utils/replyUtils");
-const Post = require("../models/posts");
-const User = require("../models/users");
-
-/* Callback 함수 사용 소스
-exports.writeReply = (req, res) => {
-    const { reply, post_id } = req.body;
-    const { user_id } = req.session;
-
-    if (!reply || reply === null) {
-        return res.status(400).json({ error: 'Reply is required.' });
-    }
-
-    if (!user_id) {
-        return res.status(401).json({ error: 'Authentication required.' });
-    }
-
-    // user_id를 사용하여 사용자 정보 조회
-    replyUtils.getUserById(user_id, (error, user) => {
-        if (error) {
-            console.error(error);
-            return res.status(500).json({ error: 'An error occurred' });
-        }
-
-        if (!user) {
-            return res.status(401).json({ error: 'User not found.' });
-        }
-
-        const reqData = {
-            post_id: mysql.escape(post_id),
-            reply: mysql.escape(reply),
-            username: mysql.escape(user.username), // 사용자 정보에서 username 사용
-            user_id: mysql.escape(user_id),
-        }
-
-        Reply.writeReply(reqData, (error, reply_id) => {
-            if (error) {
-                console.error(error);
-                return res.status(500).json({ error: 'An error occurred' });
-            }
-
-            return res.status(201).json({ message: `Upload post Success. Reply ID is ${reply_id}` });
-        });
-    });
-}
-
-exports.getUserById = (req, res) => {
-    const user_id = req.params.user_id;
-
-    replyUtils.getUserById(user_id, (error, user) => {
-        if (error) {
-            return res.status(500).json({ error: 'An error occurred' });
-        }
-
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        return res.status(200).json(user);
-    });
-}
-
-exports.getReplyByPostId = (req, res) => {
-    // const { offset, limit } = req.query;
-    const limit = parseInt(req.query.limit); // 문자열을 숫자로 변환
-    let offset = parseInt(req.query.offset); // 문자열을 숫자로 변환
-
-    const post_id = req.params.post_id;
-
-    if (!offset || isNaN(offset) || offset < 0) {
-        offset = 0; // offset이 0 또는 음수일 경우 0으로 설정
-    }
-    if (!limit || isNaN(limit) || limit < 1) {
-        return res.status(400).json({ code: 'invalid_limit' });
-    }
-
-    const totalPostsQuery = `SELECT COUNT(*) AS totalPosts FROM reply;`;
-
-    db.connection.query(totalPostsQuery, (error, results) => {
-        if (error) {
-            console.log(error);
-            console.log('reply / First 500 error');
-            return res.status(500).json({message: '내부 서버 오류'});
-        }
-
-        const totalPosts = results[0].totalPosts;
-
-        const reqData = {
-            post_id: post_id,
-            offset: offset,
-            limit: limit,
-        };
-
-        Reply.getReplyByPostId(reqData, (error, reply) => {
-            if (error) {
-                console.error('MySQL Error:', error); // MySQL 오류 로그 추가
-                console.log('reply / Second 500 error');
-                return res.status(500).json({ error: '내부 서버 오류' });
-            }
-
-            if (!reply) {
-                return res.status(404).json({ message: 'Post not found' });
-            }
-
-            return res.status(200).json({
-                totalPosts: totalPosts,
-                message: reply
-            });
-        })
-    })
-}
- */
 
 exports.writeReply = async (req, res) => {
     const { reply, post_id } = req.body;
@@ -135,16 +24,16 @@ exports.writeReply = async (req, res) => {
         }
 
         const reqData = {
-            post_id: mysql.escape(post_id),
-            reply: mysql.escape(reply),
-            username: mysql.escape(user.username), // 사용자 정보에서 username 사용
-            user_id: mysql.escape(user_id),
+            post_id: post_id,
+            reply: reply,
+            username: user.username,
+            user_id: user_id,
         }
 
         const reply_id = await Reply.writeReply(reqData);
         return res.status(201).json({ message: `Upload post Success. Reply ID is ${reply_id}` });
     } catch(error) {
-        console.error(error);
+        errorlog(error);
         return res.status(500).json({ error: 'An error occurred' });
     }
 }
@@ -167,30 +56,41 @@ exports.getUserById = async (req, res) => {
 
 exports.getReplyByPostId = async (req, res) => {
     const post_id = req.params.post_id;
+    const offset = parseInt(req.query.offset);
+    const limit = parseInt(req.query.limit);
+
+    if (offset === undefined || offset === null || isNaN(offset) || offset < 0) {
+        return res.status(400).json({ code: 'invalid_offset' });
+    }
+    if (!limit || isNaN(limit) || limit < 1) {
+        return res.status(400).json({ code: 'invalid_limit' });
+    }
 
     try {
-        const totalPostsResults = await new Promise((resolve, reject) => {
-            const totalPostsQuery = `SELECT COUNT(*) AS totalPosts FROM reply;`;
-            db.connection.query(totalPostsQuery, (error, results) => {
+        const totalReplyResults = await new Promise((resolve, reject) => {
+            const totalReplyQuery = `SELECT COUNT(*) AS totalReply FROM reply WHERE post_id = ?;`;
+            db.connection.query(totalReplyQuery, post_id, (error, results) => {
                 if (error) reject(error);
                 resolve(results);
             });
         });
 
-        const totalPosts = totalPostsResults[0].totalPosts;
+        const totalReply = totalReplyResults[0].totalReply;
 
         const reqData = {
+            offset: offset,
+            limit: limit,
             post_id: post_id,
         };
 
         const reply = await Reply.getReplyByPostId(reqData);
 
         if (!reply) {
-            return res.status(404).json({ message: 'Post not found' });
+            return res.status(404).json({ message: 'Reply not found' });
         }
 
         return res.status(200).json({
-            totalPosts: totalPosts,
+            totalReply: totalReply,
             message: reply
         });
     } catch (error) {
@@ -202,23 +102,35 @@ exports.getReplyByPostId = async (req, res) => {
 
 exports.getReplyByUserId = async (req, res) => {
     const { user_id } = req.session;
+    devlog(`[Cont] reply / getReplyByUserId user_id = ${user_id}`);
+    const offset = parseInt(req.query.offset);
+    const limit = parseInt(req.query.limit);
+
+    if (offset === undefined || offset === null || isNaN(offset) || offset < 0) {
+        return res.status(400).json({ code: 'invalid_offset' });
+    }
+    if (!limit || isNaN(limit) || limit < 1) {
+        return res.status(400).json({ code: 'invalid_limit' });
+    }
 
     if (!user_id) {
         return res.status(401).json({ error: 'Authentication required.' });
     }
 
     try {
-        const totalPostsResults = await new Promise((resolve, reject) => {
-            const totalPostsQuery = `SELECT COUNT(*) AS totalPosts FROM reply;`;
-            db.connection.query(totalPostsQuery, (error, results) => {
+        const totalReplyResults = await new Promise((resolve, reject) => {
+            const totalReplyQuery = `SELECT COUNT(*) AS totalReply FROM reply WHERE user_id = ?;`;
+            db.connection.query(totalReplyQuery, user_id, (error, results) => {
                 if (error) reject(error);
                 resolve(results);
             });
         });
 
-        const totalPosts = totalPostsResults[0].totalPosts;
+        const totalReply = totalReplyResults[0].totalReply;
 
         const reqData = {
+            offset: offset,
+            limit: limit,
             user_id: user_id,
         };
 
@@ -229,7 +141,7 @@ exports.getReplyByUserId = async (req, res) => {
         }
 
         return res.status(200).json({
-            totalPosts: totalPosts,
+            totalReply: totalReply,
             message: reply
         });
     } catch (error) {
--- models/reply.js
+++ models/reply.js
@@ -1,49 +1,16 @@
 const mysql = require('../config/database');
 const {devlog, errorlog} = require("../config/config");
 
-/* Callback 함수 사용 소스
-exports.writeReply = (reqData, callback) => {
-    const sql = `INSERT INTO reply (post_id, user_id, username, reply)
-                VALUES (${reqData.post_id}, ${reqData.user_id}, ${reqData.username}, ${reqData.reply});`;
-    mysql.connection.query(sql, (error, results) => {
-        if (error)  {
-            // 에러
-            console.error(error);
-            callback(error, null);
-        } else {
-            callback(null, results.insertId);
-        }
-    });
-}
-
-exports.getReplyByPostId = (reqData, callback) => {
-    devlog("Reply / getReplyByPostId in");
-
-    const { post_id, offset, limit } = reqData;
-    const sql = 'SELECT * FROM reply WHERE post_id = ? LIMIT ? OFFSET ?';
-    mysql.connection.query(sql, [post_id, limit, offset], (error, results) => {
-        if (error) {
-            return callback(error, null);
-        }
-
-        if (results.length === 0) {
-            return callback(null, null); // 게시글 없을 경우 null 반환
-        }
-
-        return callback(null, results);
-    });
-}
- */
-
-// 게시글쓰기
+// 댓글 쓰기
 exports.writeReply = async (reqData) => {
+    const { post_id, user_id, username, reply } = reqData;
     const sql = `INSERT INTO reply (post_id, user_id, username, reply)
-                VALUES (${reqData.post_id}, ${reqData.user_id}, ${reqData.username}, ${reqData.reply});`;
+                VALUES (?, ?, ?, ?);`;
     try {
         const results = await new Promise((resolve, reject) => {
-            mysql.connection.query(sql, (error, results) => {
+            mysql.connection.query(sql, [post_id, user_id, username, reply], (error, results) => {
                 if (error)  {
-                    console.error(error);
+                    errorlog(error);
                     return reject(error);
                 }
                 return resolve(results.insertId);
@@ -58,20 +25,19 @@ exports.writeReply = async (reqData) => {
 exports.getReplyByPostId = async (reqData) => {
     devlog("Reply / getReplyByPostId in");
 
-    const { post_id } = reqData;
-    const sql = 'SELECT * FROM reply WHERE post_id = ?;';
+    const { post_id, limit, offset } = reqData;
+    const sql = 'SELECT * FROM reply WHERE post_id = ? LIMIT ? OFFSET ?;';
 
     try {
         const results = await new Promise((resolve, reject) => {
-            mysql.connection.query(sql, post_id, (error, results) => {
+            mysql.connection.query(sql, [post_id, limit, offset], (error, results) => {
                 if (error) {
+                    errorlog(error);
                     return reject(error);
                 }
-
                 if (results.length === 0) {
                     return resolve(null); // 게시글 없을 경우 null 반환
                 }
-
                 return resolve(results);
             });
         });
@@ -81,7 +47,7 @@ exports.getReplyByPostId = async (reqData) => {
     }
 }
 
-exports.getReplyByUserId = async (reqData) => {
+exports.getReplyByUserId = async (reqData) => { /*여기하는중*/
     devlog("Reply / getReplyByUserId in");
 
     const { post_id } = reqData;
@@ -149,6 +115,7 @@ exports.getDeletedReply = async (reqData) => {
         const results = await new Promise((resolve, reject) => {
             mysql.connection.query(sql, (error, results) => {
                 if (error) {
+                    errorlog(error);
                     return reject(error);
                 }
                 return resolve(results);
