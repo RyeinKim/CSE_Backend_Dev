31819a34ce129432781f042289c6a37c4f8c4d67
RyeinKim <ryein6896@gmail.com>
2023-10-06 08:29:15 +0900

    Keep dev on S700

--- config/config.js
+++ config/config.js
@@ -1,19 +1,12 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-const dev = process.env.NODE_ENV;
-
 function devlog(message) {
-    if (dev === 'dev') console.log(message);
+    if (process.env.NODE_ENV === 'dev') console.log(message);
 }
 
 function errorlog(error) {
-    if (dev === 'dev') console.error(error);
+    if (process.env.NODE_ENV === 'dev') console.error(error);
 }
 
 module.exports = {
-    dev,
     devlog,
     errorlog,
 };
